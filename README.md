0eae82fb1de5d7fb09deda2761258c7b6736cc3a
Caroline Robinson <CRobinsonAtWork@gmail.com>
2026-09-04 07:55:12 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# docassemble.PetitionForProtectiveOrderEfilingDemo
+
+A docassemble extension.
+
+## Author
+
+Caroline Robinson, CRobinson@akcourts.gov
+
---
+++ setup.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from setuptools import setup, find_namespace_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.PetitionForProtectiveOrderEfilingDemo',
+      version='26.09.04',
+      description=('A docassemble extension.'),
+      long_description='# docassemble.PetitionForProtectiveOrderEfilingDemo\n\nA docassemble extension.\n\n## Author\n\nCaroline Robinson, CRobinson@akcourts.gov\n\n',
+      long_description_content_type='text/markdown',
+      author='Caroline Robinson',
+      author_email='CRobinson@akcourts.gov',
+      license='MIT',
+      url='https://docassemble.org',
+      packages=find_namespace_packages(),
+      install_requires=['docassemble.AKA2JBranding @ git+https://github.com/A2JatAKCourts/docassemble-AKA2JBranding.git@main', 'docassemble.ALToolbox @ git+https://github.com/SuffolkLITLab/docassemble-ALToolbox.git@main', 'docassemble.AssemblyLine @ git+https://github.com/SuffolkLITLab/docassemble-AssemblyLine.git@main', 'docassemble.GithubFeedbackForm @ git+https://github.com/SuffolkLITLab/docassemble-GithubFeedbackForm.git@main'],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/PetitionForProtectiveOrderEfilingDemo/', package='docassemble.PetitionForProtectiveOrderEfilingDemo'),
+     )
